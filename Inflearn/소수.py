e9d44616e04d53dc269ad942e526c01988c71759
Noh Hyun Jin <nohhyunjin100@gmail.com>
2022-01-17 23:58:29 +0900

    20220117_prime_list

--- Inflearn/소수.py
+++ Inflearn/소수.py
@@ -1,1 +1,13 @@
 n = int(input())
+
+def print_list(n):
+    ch = [True] * n
+    m = int(n ** 0.5)
+    
+    for i in range(2, m + 1):
+        if ch[i] == True:
+            for ch[j] in range(2 * i, n, i):
+                ch[j] = False
+    return [ i for i in range(2, n) if ch[i] == True ]
+
+len(prime_list(n))
