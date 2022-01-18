28a86e1f8e0ed537394cc14d22cc39445b5105ab
Noh Hyun Jin <nohhyunjin100@gmail.com>
2022-01-19 00:07:34 +0900

    20220119 섹션2 10번문제까지

---
+++ Inflearn/뒤집은소수.py
@@ -0,0 +1,29 @@
+#N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 수가 소수이면 그 수를 출력하는 프로그램을 작성하세요. 
+#예를 들어 32를 뒤집으면 23이고, 23은 소수이다. 그러면 23을 출력한다. 
+#단 910를 뒤집으면 19로 숫자화 해야 한다. 첫 자리부터의 연속된 0은 무시한다.
+#뒤집는 함수인 def reverse(x) 와 소수인지를 확인하는 함수 def isPrime(x)를 반드시 작성하여 프로그래밍 한다.
+
+n = int(input())
+a = list(map(int, input().split()))
+
+def reverse(x):
+    rev = 0
+    while x > 0:
+        s = x % 10
+        rev = rev * 10 + s
+        x = x // 10
+    return rev
+
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, x):
+        if x % 1 == 0:
+            return False
+    return True
+
+for x in a :
+    rev_num = reverse(x)
+    if isPrime(rev_num):
+        print(rev_num, end = "")
+
