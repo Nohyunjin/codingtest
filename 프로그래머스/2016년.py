66350a93ffdb6b67915597ad1ba062f290e07bdd
Noh Hyun Jin <nohhyunjin100@gmail.com>
2022-02-10 21:19:59 +0900

    2022/02/10 코드 가시성 향상

--- 프로그래머스/2016년.py
+++ 프로그래머스/2016년.py
@@ -7,5 +7,5 @@
 import datetime
 
 def solution(a, b):
-    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    t = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
     return t[datetime.datetime(2016, a, b).weekday()]
